81f04b204d98cf07dfbc8d074fc479f85386efe2
wim wolvers <ahcwolvers@gmail.com>
2023-06-25 16:55:54 +0200

    Incremental commit while refactoring. This commit will be squashed.

--- app/src/main/java/com/androidopengl/app/opengl/charsequence/CharSequenceOpenGLObject.kt
+++ app/src/main/java/com/androidopengl/app/opengl/charsequence/CharSequenceOpenGLObject.kt
@@ -1,12 +1,46 @@
 package com.androidopengl.app.opengl.charsequence
 
 import android.content.Context
+import android.opengl.GLES20
+import com.androidopengl.app.loadShaderFromFile
+import com.androidopengl.app.logError
+import com.androidopengl.app.logEvent
 import com.androidopengl.app.opengl.OpenGLObject
 
-class CharSequenceOpenGLObject(context: Context, color: FloatArray, coordinates: FloatArray): OpenGLObject(coordinates, context) {
+class CharSequenceOpenGLObject(private val context: Context, color: FloatArray, coordinates: FloatArray): OpenGLObject(coordinates, context) {
+
+    private var programHandle: Int = -1
+
+    init {
+        createProgram()
+    }
 
     override fun createProgram() {
-        TODO("Not yet implemented")
+        val fragmentShaderHandle = loadShaderFromFile(context,
+            GLES20.GL_FRAGMENT_SHADER, "shaders/charsequence/charsequence.fshader"
+        )
+
+        val vertexShaderHandle = loadShaderFromFile(context,
+            GLES20.GL_VERTEX_SHADER, "shaders/charsequence/charsequence.vshader"
+        )
+
+        programHandle = GLES20.glCreateProgram()
+
+        logEvent(
+            event = "programHandle: $programHandle\n"
+        )
+
+        if (programHandle == 0) {
+            logError(
+                error = "Something went wrong while calling glCreateProgram()..."
+            )
+            return
+        }
+
+        GLES20.glAttachShader(programHandle, vertexShaderHandle)
+        GLES20.glAttachShader(programHandle, fragmentShaderHandle)
+        GLES20.glLinkProgram(programHandle)
+        GLES20.glUseProgram(programHandle)
     }
 
 }
