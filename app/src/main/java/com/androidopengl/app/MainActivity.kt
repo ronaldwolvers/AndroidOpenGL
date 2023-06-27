44be0aba4f5e9f534f9e7ac618c27ac1a61467ed
Ronald Wolvers <ahcwolvers@gmail.com>
2023-06-27 15:50:00 +0200

    Incremental commit

--- app/src/main/java/com/androidopengl/app/MainActivity.kt
+++ app/src/main/java/com/androidopengl/app/MainActivity.kt
@@ -44,6 +44,24 @@ class MainActivity : AppCompatActivity() {
 
         glSurfaceView.renderMode = renderMode.renderMode
 
+        class AndroidOpenGLTextChanged(val operation: (()-> Unit)): TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun afterTextChanged(s: Editable?) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                try {
+                    operation.invoke()
+                } catch (e: Exception) {
+                    logError("Something went wrong: ${e.message}")
+                    e.printStackTrace()
+                }
+
+                logEvent("Requesting render...")
+                glSurfaceView.requestRender()
+            }
+
+        }
+
         nearF = findViewById(R.id.edit_text_frustum_near_f)
         nearF.setText(DEFAULT_NEAR_F.toString())
         nearF.addTextChangedListener(object: TextWatcher {
